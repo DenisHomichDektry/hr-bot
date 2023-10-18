1252e8eeabe1155061c44da9668445399a78bec1
Denis <takoiwebile@gmail.com>
2023-10-18 15:18:40 +0200

    basic telegraf setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppUpdate } from './app.update';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TelegrafModule.forRoot({
+      token: process.env.TELEGRAM_TOKEN,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppUpdate],
 })
 export class AppModule {}
---
+++ src/app.update.ts
@@ -0,0 +1,34 @@
+import { Update, Ctx, Start, Help, On, Hears } from 'nestjs-telegraf';
+import { SceneContext } from 'telegraf/typings/scenes';
+
+@Update()
+export class AppUpdate {
+  @Start()
+  async start(@Ctx() ctx: SceneContext) {
+    await ctx.reply('Welcome');
+    await ctx.reply('2+2 = ?', {
+      reply_markup: {
+        keyboard: [
+          [{ text: 'Onboarding' }],
+          [{ text: 'Knowledge Base' }],
+          [{ text: 'Feedback' }],
+        ],
+      },
+    });
+  }
+
+  @Help()
+  async help(@Ctx() ctx: SceneContext) {
+    await ctx.reply('Send me a sticker');
+  }
+
+  @On('sticker')
+  async on(@Ctx() ctx: SceneContext) {
+    await ctx.reply('👍');
+  }
+
+  @Hears('hi')
+  async hears(@Ctx() ctx: SceneContext) {
+    await ctx.reply('Hey there');
+  }
+}
