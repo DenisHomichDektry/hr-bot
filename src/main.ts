7295ede62a150236aae6ed069ed77660075d1980
DenisHomichDektry <73819600+DenisHomichDektry@users.noreply.github.com>
2023-11-11 15:44:00 +0100

    Dev (#33)
    * derive url from env
    
    * enables https

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import 'dotenv/config';
+import * as fs from 'fs';
 
 import { AppModule } from './app.module';
+import * as process from 'process';
+
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/hr-bot.ddns.net/fullchain.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/hr-bot.ddns.net/privkey.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions: process.env.ENV === 'development' ? null : httpsOptions,
+  });
   app.enableCors({
     origin: [process.env.WEB_APP_URL, 'http://localhost:5173'],
   });
